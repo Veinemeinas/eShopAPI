9d5ca02138da064993a2b0ad2205a2ac25c01002
Donatas Domres <donatas.domres@gmail.com>
2022-09-09 22:15:18 +0300

    Added Context and Migration

---
+++ eShopAPI/Context/eShopContext.cs
@@ -0,0 +1,11 @@
+﻿using eShopAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopAPI.Context
+{
+    public class eShopContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public eShopContext(DbContextOptions options) : base(options) { }
+    }
+}
